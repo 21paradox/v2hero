c5e1735d511ab94e13e2199e80ede8bf0eeb0011
shiwenzhe <shiwenzhe@lixiang.com>
2021-10-08 09:17:37 +0800

    fix build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 cd /v2raybin
 
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$PORT" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
+echo -e -n "$CONFIG_JSON1" > /v2raybin/config.json
+echo -e -n "$PORT" >> /v2raybin/config.json
+echo -e -n "$CONFIG_JSON2" >> /v2raybin/config.json
+echo -e -n "$UUID" >> /v2raybin/config.json
+echo -e -n "$CONFIG_JSON3" >> /v2raybin/config.json
+
+cat /v2raybin/config.json
 
 ./v2ray
