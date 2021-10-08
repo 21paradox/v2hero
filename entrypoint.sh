52d2e28ee0a8561da6a01a2f0da7e92ce6a75c5c
shiwenzhe <shiwenzhe@lixiang.com>
2021-10-08 09:27:13 +0800

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env sh
+set -e
+
 cd /v2raybin
+
+echo "begin"
 
 echo -e -n "$CONFIG_JSON1" > /v2raybin/config.json
 echo -e -n "$PORT" >> /v2raybin/config.json
@@ -6,6 +11,7 @@ echo -e -n "$CONFIG_JSON2" >> /v2raybin/config.json
 echo -e -n "$UUID" >> /v2raybin/config.json
 echo -e -n "$CONFIG_JSON3" >> /v2raybin/config.json
 
+echo "end"
 cat /v2raybin/config.json
 
 ./v2ray
